bb7fb23433dc0c090be72d705f8023a020c18273
almohajire <yahya773@gmail.com>
2018-11-23 12:41:59 +0100

    Update mining.blade.php

--- resources/views/users/pages/links/mining.blade.php
+++ resources/views/users/pages/links/mining.blade.php
@@ -1,6 +1,8 @@
 @extends('users.layouts.app')
 <link href="{{ asset('users/css2/test.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 @section('content')
 
 
