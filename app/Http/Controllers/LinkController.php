6713812acc949393076bf144860330c7e5b0d484
mrhili <mohamedaminemrhili@users.noreply.github.com>
2018-12-19 02:28:31 +0100

    Hasching changes

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -200,7 +200,7 @@ class LinkController extends Controller
 
             $user = Auth::user();
 
-            $hash = Hash::make( $user->id. str_random(15) );
+            $hash = sha256( $user->id. str_random(15) );
 
             Session::put('lastHash', $hash);
 
